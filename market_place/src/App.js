2d767af52451c9fe9a3f59c1b1240441129230a3
pc <samsontim@hotmail.fr>
2019-11-15 13:44:08 +0100

    ajout UI light

--- market_place/src/App.js
+++ market_place/src/App.js
@@ -8,40 +8,65 @@ class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { account: '' }
+    this.state = { account: '', demandes : '' }
   }
 
-  componentWillMount() {
-
- 
+  componentDidMount() {
     this.loadBlockchainData()
   }
 
   async loadBlockchainData() {
     const web3 = new Web3(Web3.givenProvider)
     const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
+   
 
     const networkId = await web3.eth.net.getId()
     const networkData = marketPlace.networks[networkId]
 
     if(networkData) {
       const contrat = new web3.eth.Contract(marketPlace.abi, networkData.address)
-     // const demandes = await contrat.methods.getDemandes().call()
-      console.log(await contrat.methods.demandes("0x6b7eb67f41dcaae38dfa61eba0bd37dc4ab91d5f").call())
-     
+      const demandes = await contrat.methods.getAllDemandes().call()
+      this.setState({ account: accounts[0], demandes : demandes })
     }else {
       window.alert('MarketPlace contract not deployed to detected network id:',networkId)
       }
   }
 
- 
+  renderOffres = () => {
+    let {demandes} = this.state
+    let res = []
+    const demandeMapping = ["Remuneration","Débute le","Description","Statut","Reputation minimum","Ayant Postulés"]
+    
+    if(typeof demandes == 'object'){
+      for(let idx = 0;idx < demandes[0].length; idx++){
+        let lis = Object.keys(demandes).map( key => {
+          let demande = demandes[key][idx]
+          
+          if(typeof demande == 'object'){
+            let clients = demande.map(client => {
+              return <li>{client}</li>
+            })
+            return <li>{demandeMapping[key]} : <ul>{clients}</ul></li>
+          }
+          
+          return  <li>{demandeMapping[key]} : {demande}</li> 
+        });
+        res.push(<span className="list-type2"><h4 className="offresh4">Offre #{idx+1}</h4><ul>{lis}</ul></span>)
+      
+         
+      }
+    }
+    return res
+    
+  }
+
 
   render() {
+    let { account } = this.state
     return (
       <div className="container">
-        <h1>Hello, World!</h1>
-        <p>Your account: {this.state.account}</p>
+        <h1 className="offresH1">Liste d'offres</h1>
+        <div>{this.renderOffres()}</div>
       </div>
     );
   }
